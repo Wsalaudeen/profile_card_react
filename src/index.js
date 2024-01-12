2e9dc11702cbab66f509493e5f9e4db39f7d17f9
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-01-12 10:38:14 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="card">
+      <Avatar photoName="olanrewaju.jpg" name="profile-picture" />
+      <div className="data">
+        <Intro
+          name="Salaudeen olanrewaju"
+          text="full-stack web developer and teacher at udemy.When not coding or preparing a course i like to play board games to cook and eat,or to enjoy portuguese sun."
+        />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Avatar({ photoName, name }) {
+  return (
+    <header>
+      <img src={photoName} alt={name} className="avatar" />
+    </header>
+  );
+}
+
+function Intro({ name, text }) {
+  return (
+    <main>
+      <h1>{name}</h1>
+      <p>{text}</p>
+    </main>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="Html+Css" emoji="💪" color="orangered" />
+      <Skill skill="Web dev" emoji="💪" color="lightblue" />
+      <Skill skill="Javascript" emoji="💪" color="yellow" />
+      <Skill skill="Svelte" emoji="💪" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
